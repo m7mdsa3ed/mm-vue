b12165e16ec0c8f34705f9ead982f6a7bc23a0a7
M7mDSa3eD <m7md.sa3ed@hotmail.com>
2022-01-22 18:21:57 +0200

    pwa updates

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
+  <div
+    class="alert alert-primary d-flex align-items-center justify-content-center"
+  >
+    <p class="mb-0 me-4 lead">An update is available</p>
+    <button class="btn btn-dark btn-sm" @click="refreshApp">Update</button>
+  </div>
+
   <RouterView />
 </template>
 
 <script>
 import { mapActions } from "vuex";
+import ServiceWorkerUpdate from "@/mixins/swUpdate";
+
 export default {
+  mixins: [ServiceWorkerUpdate],
+
   beforeCreate() {
     this.$store.commit("restoreState");
   },
