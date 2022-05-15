8e3e8831f4eab95ce37d39c2d119b811d975d044
M7mDSa3eD <m7md.sa3ed@hotmail.com>
2022-05-15 16:05:10 +0200

    check pwa updates, test5

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,5 @@
 <template>
   <div>
     <p>Dashboard is work in progress.</p>
-
-    <p>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti ea
-      aliquam ullam et unde aspernatur recusandae, amet, at corrupti natus
-      eveniet illo, suscipit maxime necessitatibus id ex voluptatibus voluptate
-      ut.
-    </p>
   </div>
 </template>
