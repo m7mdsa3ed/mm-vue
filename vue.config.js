dc654abb5c6344c33e311567ea4e633cd6730b8a
M7mDSa3eD <m7md.sa3ed@hotmail.com>
2022-05-15 15:02:43 +0200

    check pwa updates, test2

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,11 @@
 module.exports = {
   // publicPath: process.env.NODE_ENV === "production" ? "/fin" : "/",
   filenameHashing: false,
-  productionSourceMap: false,
   pwa: {
     name: "Money Management",
     themeColor: "#000000",
     workboxOptions: {
       exclude: [/\.htaccess$/],
-      skipWaiting: true,
     },
   },
 };
