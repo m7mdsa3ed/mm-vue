9293183f0151a95cec777657bb5004e1426394fc
Mohamed Saeed <m7md.sa3ed@hotmail.com>
2023-01-15 22:17:55 +0200

    fix pwa icon

--- vite-pwa.config.js
+++ vite-pwa.config.js
@@ -5,20 +5,10 @@ const pwaOptions = {
   manifest: {
     name: 'Money Man',
     short_name: 'MM',
-    theme_color: '#000000',
+    theme_color: '#FFFFFF',
     icons: [
       {
-        src: 'img/icons/android-chrome-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
-      },
-      {
-        src: 'img/icons/android-chrome-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-      },
-      {
-        src: 'img/icons/android-chrome-512x512.png',
+        src: 'img/icons/icon.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'any maskable',
