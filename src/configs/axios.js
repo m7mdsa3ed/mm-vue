e7d81615c515193e3394f241b05de2f4b645de2e
Mohamed Saeed <m7md.sa3ed@hotmail.com>
2023-01-30 00:37:22 -0800

    change backend endpoint url env key

--- src/configs/axios.js
+++ src/configs/axios.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 import store from "./../store";
 
-const baseURL =
-import.meta.env.MODE == "development"
-  ? import.meta.env.VITE_API_BASEURL_LOCAL
-  : import.meta.env.VITE_API_BASEURL;
+const baseURL = import.meta.env.VITE_API_BASEURL;
 
 // Axios configuration
 axios.defaults.baseURL = baseURL + "/api";
