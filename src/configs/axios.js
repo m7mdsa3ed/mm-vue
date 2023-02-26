30322f640bdcb222038e76f083c63b583828c33b
Mohamed Saeed <m7md.sa3ed@hotmail.com>
2023-02-26 14:53:56 +0200

    update api baseurl

--- src/configs/axios.js
+++ src/configs/axios.js
@@ -4,5 +4,5 @@ import store from "./../store";
 const baseURL = import.meta.env.VITE_API_BASEURL;
 
 // Axios configuration
-axios.defaults.baseURL = baseURL + "/api";
+axios.defaults.baseURL = baseURL;
 axios.defaults.headers.Authorization = `Bearer ${store.state.auth.token}`;
