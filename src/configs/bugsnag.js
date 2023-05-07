15cf5d3dbdd21e98a9e2069c1a1297781c4cfdba
Mohamed Saeed <m7md.sa3ed@hotmail.com>
2023-05-07 23:21:44 +0300

    fix bugsnag

--- src/configs/bugsnag.js
+++ src/configs/bugsnag.js
@@ -1,5 +1,5 @@
 import Bugsnag from '@bugsnag/js'
 
-if (import.meta.env.VITE_BUGSNAG_ENABLED) {
+if (eval(import.meta.env.VITE_BUGSNAG_ENABLED)) {
   Bugsnag.start(import.meta.env.VITE_BUGSNAG_API_KEY)
 }
