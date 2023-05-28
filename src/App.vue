b2ffdd072a81130002b2617a917a6eab7fa1dfbc
Mohamed Saeed <m7md.sa3ed@hotmail.com>
2023-05-29 01:02:59 +0300

    main component to composition api

--- src/App.vue
+++ src/App.vue
@@ -14,71 +14,64 @@
   </div>
 </template>
 
-<script>
+<script setup>
+import { onBeforeMount, onMounted } from "vue";
+import { useStore } from "vuex";
 import ReloadServiceWorkerVue from "./components/ReloadServiceWorker.vue";
 
-export default {
-  components: {
-    ReloadServiceWorkerVue,
-  },
+const { commit, dispatch, state } = useStore();
 
-  beforeCreate() {
-    this.$store.commit("restoreState");
-  },
+onBeforeMount(() => {
+  commit("restoreState");
+});
 
-  created() {
-    this.syncColorSchema();
-  },
+onMounted(() => {
+  syncColorSchema();
 
-  mounted() {
-    this.$store.dispatch("app/fetchAppInfo");
+  dispatch("app/fetchAppInfo");
 
-    if (this.$store.state.auth.user) {
-      this.$store.dispatch("app/fetchAll");
+  if (state.auth.user) {
+    dispatch("app/fetchAll");
+  }
+});
+
+const changeSchema = (schema = "light") => {
+  const html = document.documentElement;
+
+  html.setAttribute("data-bs-theme", schema);
+
+  const bodyBackgroundColor =
+    getComputedStyle(html).getPropertyValue("--bs-body-bg");
+
+  document
+    .querySelector('meta[name="theme-color"]')
+    .setAttribute("content", bodyBackgroundColor.trim());
+};
+
+const syncColorSchema = () => {
+  const systemDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
+
+  systemDarkMode.addEventListener("change", (e) => {
+    const schema = e.matches ? "dark" : "light";
+
+    changeSchema(schema);
+  });
+
+  const schema = systemDarkMode.matches ? "dark" : "light";
+
+  changeSchema(schema);
+
+  dispatch("app/changeAppSchema", schema);
+
+  window.addEventListener("keydown", (e) => {
+    if (e.ctrlKey && e.key == "Enter") {
+      const element = e.target;
+
+      // Get form from [form] attribute or the closest one
+      const form = element.form || element.closest("form");
+
+      form.dispatchEvent(new Event("submit"));
     }
-
-    this.submitFormShortcut();
-  },
-
-  methods: {
-    syncColorSchema() {
-      const changeSchema = (schema = "light") => {
-        document.documentElement.setAttribute("data-bs-theme", schema);
-
-        this.$store.dispatch("app/changeAppSchema", schema);
-
-        const bodyBackgroundColor = getComputedStyle(
-          document.documentElement
-        ).getPropertyValue("--body-bg");
-
-        document
-          .querySelector('meta[name="theme-color"]')
-          .setAttribute("content", bodyBackgroundColor.trim());
-      };
-
-      const systemDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
-
-      systemDarkMode.addEventListener(
-        "change",
-        (e) => e.matches && changeSchema("dark")
-      );
-
-      changeSchema(systemDarkMode.matches ? "dark" : "light");
-    },
-
-    submitFormShortcut() {
-      // Ctrl + Enter to submit the form
-      window.addEventListener("keydown", (e) => {
-        if (e.ctrlKey && e.key == "Enter") {
-          const element = e.target;
-
-          // Get form from [form] attribute or the closest one
-          const form = element.form || element.closest("form");
-
-          form.dispatchEvent(new Event("submit"));
-        }
-      });
-    },
-  },
+  });
 };
 </script>
