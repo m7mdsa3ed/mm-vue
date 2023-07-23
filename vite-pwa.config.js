9f6224231a32b7c225c1ffe793f5751595a9cd65
Mohamed Saeed <m7md.sa3ed@hotmail.com>
2023-07-23 18:25:12 +0300

    update logo using bing image creator

--- vite-pwa.config.js
+++ vite-pwa.config.js
@@ -1,66 +1,95 @@
 const pwaOptions = {
-  mode: 'development',
-  base: '/',
-  includeAssets: ['favicon.svg'],
+  mode: "production",
+  base: "/",
+  includeAssets: ["favicon.svg"],
   manifest: {
-    name: 'Money Man',
-    short_name: 'MM',
-    theme_color: '#000000',
-    background_color: '#000000',
+    name: "Money Managements",
+    short_name: "Money Managements",
+    theme_color: "#000000",
+    background_color: "#000000",
     icons: [
       {
-        src: 'img/icons/icon.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'any maskable',
+        src: "img/icons/icon-72x72.png",
+        sizes: "72x72",
+        type: "image/png",
+        purpose: "maskable any",
+      },
+      {
+        src: "img/icons/icon-96x96.png",
+        sizes: "96x96",
+        type: "image/png",
+        purpose: "maskable any",
+      },
+      {
+        src: "img/icons/icon-128x128.png",
+        sizes: "128x128",
+        type: "image/png",
+        purpose: "maskable any",
+      },
+      {
+        src: "img/icons/icon-144x144.png",
+        sizes: "144x144",
+        type: "image/png",
+        purpose: "maskable any",
+      },
+      {
+        src: "img/icons/icon-152x152.png",
+        sizes: "152x152",
+        type: "image/png",
+        purpose: "maskable any",
+      },
+      {
+        src: "img/icons/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable any",
+      },
+      {
+        src: "img/icons/icon-384x384.png",
+        sizes: "384x384",
+        type: "image/png",
+        purpose: "maskable any",
+      },
+      {
+        src: "img/icons/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable any",
       },
     ],
-    "shortcuts": [
-      {
-        "name": "Transactions",
-        "short_name": "Transactions",
-        "url": "/transactions",
-      },
-      {
-        "name": "Accounts",
-        "short_name": "Accounts",
-        "url": "/accounts",
-      },
-    ]
   },
   devOptions: {
-    enabled: process.env.SW_DEV === 'true',
+    enabled: process.env.SW_DEV === "true",
     /* when using generateSW the PWA plugin will switch to classic */
-    type: 'module',
-    navigateFallback: 'index.html',
+    type: "module",
+    navigateFallback: "index.html",
   },
+};
+
+const replaceOptions = { __DATE__: new Date().toISOString() };
+const claims = process.env.CLAIMS === "true";
+const reload = process.env.RELOAD_SW === "true";
+const selfDestroying = process.env.SW_DESTROY === "true";
+
+if (process.env.SW === "true") {
+  pwaOptions.srcDir = "src";
+  pwaOptions.filename = claims ? "claims-sw.ts" : "prompt-sw.ts";
+  pwaOptions.strategies = "injectManifest";
+  pwaOptions.manifest.name = "PWA Inject Manifest";
+  pwaOptions.manifest.short_name = "PWA Inject";
 }
 
-const replaceOptions = { __DATE__: new Date().toISOString() }
-const claims = process.env.CLAIMS === 'true'
-const reload = process.env.RELOAD_SW === 'true'
-const selfDestroying = process.env.SW_DESTROY === 'true'
-
-if (process.env.SW === 'true') {
-  pwaOptions.srcDir = 'src'
-  pwaOptions.filename = claims ? 'claims-sw.ts' : 'prompt-sw.ts'
-  pwaOptions.strategies = 'injectManifest'
-  pwaOptions.manifest.name = 'PWA Inject Manifest'
-  pwaOptions.manifest.short_name = 'PWA Inject'
-}
-
-if (claims)
-  pwaOptions.registerType = 'autoUpdate'
+if (claims) pwaOptions.registerType = "autoUpdate";
 
 if (reload) {
   // @ts-expect-error overrides
-  replaceOptions.__RELOAD_SW__ = 'true'
+  replaceOptions.__RELOAD_SW__ = "true";
 }
 
 if (selfDestroying) {
-  pwaOptions.selfDestroying = selfDestroying
+  pwaOptions.selfDestroying = selfDestroying;
 }
 
 pwaOptions.mode = process.env.NODE_ENV;
 
-module.exports = pwaOptions
+module.exports = pwaOptions;
