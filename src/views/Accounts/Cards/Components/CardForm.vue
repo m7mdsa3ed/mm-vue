f466bf0883363f844f4e1079bef4a9d4023a4ae4
Mohamed Saeed <m7md.sa3ed@hotmail.com>
2023-11-04 19:06:30 +0200

    fix add card form layout

--- src/views/Accounts/Cards/Components/CardForm.vue
+++ src/views/Accounts/Cards/Components/CardForm.vue
@@ -27,44 +27,53 @@ watch(formCard.value, (to) => {
 </script>
 
 <template>
-  <div class="d-flex flex-column gap-3">
-    <div class="d-flex gap-3">
-      <div class="form-floating w-100">
-        <input type="text" v-model="formCard.ccName" name="cc-name" max="21" class="form-control" placeholder=" "/>
-        <label> Name </label>
+  <div class="d-flex flex-column gap-3 flex-wrap">
+    <div class="row g-3">
+      <div class="col-12 col-lg-6">
+        <div class="form-floating">
+          <input type="text" v-model="formCard.ccName" name="cc-name" max="21" class="form-control" placeholder=" "/>
+          <label> Name </label>
+        </div>
       </div>
 
-      <div class="form-floating">
-        <input type="text" v-model="formCard.ccType" name="cc-exp-year" max="4" class="form-control"
-               placeholder=" "/>
-        <label> Card Type </label>
-      </div>
-    </div>
-    <div class="d-flex gap-3">
-      <div class="d-flex gap-3 w-100">
-        <div class="form-floating w-100">
-          <input type="number" v-model="formCard.ccNumber" name="cc-number" max="16" class="form-control"
+      <div class="col-12 col-md-6 col-lg-3">
+        <div class="form-floating">
+          <input type="text" v-model="formCard.ccType" name="cc-exp-year" max="4" class="form-control"
                  placeholder=" "/>
-          <label> Card Number </label>
+          <label> Card Type </label>
         </div>
+      </div>
 
-        <div class="form-floating">
-          <input type="number" v-model="formCard.ccCsc" name="cc-cvv" max="4" class="form-control" placeholder=" "/>
-          <label> CVV </label>
-        </div>
-
-        <div class="form-floating">
-          <input type="month" v-model="formCard.ccExp" name="cc-exp" max="2" class="form-control" placeholder=" "/>
-          <label> Expiration Date </label>
-        </div>
-
+      <div class="col-12 col-md-6 col-lg-3">
         <div class="form-floating">
           <input type="text" v-model="formCard.ccBrand" name="cc-exp-year" max="4" class="form-control"
                  placeholder=" "/>
           <label> Brand </label>
         </div>
-
       </div>
+      
+      <div class="col-12 col-lg-7">
+        <div class="form-floating">
+          <input type="number" v-model="formCard.ccNumber" name="cc-number" max="16" class="form-control"
+                 placeholder=" "/>
+          <label> Card Number </label>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-6 col-lg-2">
+        <div class="form-floating">
+          <input type="number" v-model="formCard.ccCsc" name="cc-cvv" max="4" class="form-control" placeholder=" "/>
+          <label> CVV </label>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-6 col-lg-3">
+        <div class="form-floating">
+          <input type="month" v-model="formCard.ccExp" name="cc-exp" max="2" class="form-control" placeholder=" "/>
+          <label> Expiration Date </label>
+        </div>
+      </div>
+
     </div>
 
     <button class="btn btn-danger w-100" @click="saveCard">
