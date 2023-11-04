f03f26e5e7fdc0004bd0c6bdecf54e5b341c1dc7
Mohamed Saeed <m7md.sa3ed@hotmail.com>
2023-11-04 15:19:16 +0200

    enable https for vite

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,38 @@
-import { defineConfig } from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import vue from '@vitejs/plugin-vue'
 import path from 'path';
-import { VitePWA } from 'vite-plugin-pwa'
+import {VitePWA} from 'vite-plugin-pwa'
 import pwaOptions from './vite-pwa.config';
+import fs from 'fs';
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    VitePWA(pwaOptions),
-  ],
-  server: {
-    port: 8080
+export default defineConfig(({ mode  }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  const httpsConfigs = env.HTTPS_ENABLED ? {
+    key: fs.readFileSync(env.HTTPS_KEY_PATH),
+    cert: fs.readFileSync(env.HTTPS_CERT_PATH),
+  } : false
+
+  return {
+    plugins: [
+      vue(),
+      VitePWA(pwaOptions),
+    ],
+      server: {
+    port: 8080,
+      https: httpsConfigs,
   },
-  build: {
-    sourcemap: true,
-  },
-  resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
-    alias: [
-      {
-        find: '@',
-        replacement: path.resolve(__dirname, 'src')
-      }
-    ]
-  },
+    build: {
+      sourcemap: true,
+    },
+    resolve: {
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+        alias: [
+        {
+          find: '@',
+          replacement: path.resolve(__dirname, 'src')
+        }
+      ]
+    },
+  }
 });
