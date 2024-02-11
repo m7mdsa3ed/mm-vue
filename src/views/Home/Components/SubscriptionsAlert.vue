06cc3170dd233a074ec3ae90c11d4f26f9b51916
Mohamed Saeed <m7md.sa3ed@hotmail.com>
2024-02-11 11:58:56 +0200

    fix subscription ui on hp

--- src/views/Home/Components/SubscriptionsAlert.vue
+++ src/views/Home/Components/SubscriptionsAlert.vue
@@ -10,53 +10,54 @@
       </p>
 
       <div class="d-flex flex-column gap-2">
-        <div class="box bg-main d-flex justify-content-between">
+        <div class="d-flex flex-column gap-3 justify-content-between">
           <template
             v-for="subscription in subscriptions"
             :key="subscription.id"
           >
-            <div>
-              <div class="d-flex gap-2 align-items-center">
-                {{ subscription.name }}
-
+            <div class="box bg-main">
+              <div>
                 <div class="d-flex gap-2 align-items-center">
-                  <span
-                    v-if="subscription.auto_renewal"
-                    class="small text-uppercase fw-bold badge bg-success"
-                    title="Auto renewal"
-                  >
-                    <i class="fa-fw fas fa-refresh"></i>
-                  </span>
+                  {{ subscription.name }}
 
-                  <span
-                    v-if="!subscription.can_cancel"
-                    class="small text-uppercase fw-bold badge bg-secondary"
-                    title="Un-cancelable"
-                  >
+                  <div class="d-flex gap-2 align-items-center">
+                    <span
+                      v-if="subscription.auto_renewal"
+                      class="small text-uppercase fw-bold badge bg-success"
+                      title="Auto renewal"
+                    >
+                      <i class="fa-fw fas fa-refresh"></i>
+                    </span>
+
+                    <span
+                      v-if="!subscription.can_cancel"
+                      class="small text-uppercase fw-bold badge bg-secondary"
+                      title="Un-cancelable"
+                    >
+
                     <i class="fa-fw fas fa-cancel"></i>
                   </span>
 
-                  <span
-                    v-if="!subscription.is_active"
-                    class="small text-uppercase fw-bold badge bg-danger"
-                    title="Un-cancelable"
-                  >
-                    <i class="fa-fw fas fa-lock"></i>
-                  </span>
+                    <span
+                      v-if="!subscription.is_active"
+                      class="small text-uppercase fw-bold badge bg-danger"
+                      title="Un-cancelable"
+                    >
+                      <i class="fa-fw fas fa-lock"></i>
+                    </span>
+                  </div>
                 </div>
+
+                <p class="small text-muted mb-0">
+                  {{ subscription.auto_renewal ? 'Renewal' : "Expiration" }} date: {{ (subscription.expires_at) }}
+                </p>
+
+                <p class="small text-muted mb-0">
+                  Remaining Days: {{ subscription.remaining_days }} day(s)
+                </p>
+
+                {{ money(subscription.amount) }}
               </div>
-
-              <p class="small text-muted mb-0">
-                {{ subscription.auto_renewal ? 'Renewal' : "Expiration" }} date: {{ (subscription.expires_at) }}
-              </p>
-
-              <p class="small text-muted mb-0">
-                Remaining Days: {{ subscription.remaining_days }} day(s)
-              </p>
-            </div>
-
-            <div>
-              {{ money(subscription.amount) }}
             </div>
           </template>
         </div>
