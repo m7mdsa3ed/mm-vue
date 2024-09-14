f73e84a23402ad0620390ce6c9799932a83462bc
Mohamed Saeed <m7md.sa3ed@hotmail.com>
2024-09-14 15:49:40 +0300

    fix balance chart and remove the animation

--- src/views/Home/Components/Charts/BalanceChart.vue
+++ src/views/Home/Components/Charts/BalanceChart.vue
@@ -13,45 +13,6 @@ const props = defineProps({
   }
 })
 
-const getAnimation = (data) => {
-  const totalDuration = 1000;
-
-  const delayBetweenPoints = totalDuration / data.length;
-
-  const previousY = (ctx) => ctx.index === 0
-    ? ctx.chart.scales.y.getPixelForValue(100)
-    : ctx.chart.getDatasetMeta(ctx.datasetIndex).data[ctx.index - 1].getProps(['y'], true).y;
-
-  return {
-    x: {
-      type: 'number',
-      easing: 'linear',
-      duration: delayBetweenPoints,
-      from: NaN, // the point is initially skipped
-      delay(ctx) {
-        if (ctx.type !== 'data' || ctx.xStarted) {
-          return 0;
-        }
-        ctx.xStarted = true;
-        return ctx.index * delayBetweenPoints;
-      }
-    },
-    y: {
-      type: 'number',
-      easing: 'linear',
-      duration: delayBetweenPoints,
-      from: previousY,
-      delay(ctx) {
-        if (ctx.type !== 'data' || ctx.yStarted) {
-          return 0;
-        }
-        ctx.yStarted = true;
-        return ctx.index * delayBetweenPoints;
-      }
-    }
-  };
-}
-
 const getConfig = () => {
   const balance = [];
 
@@ -68,8 +29,6 @@ const getConfig = () => {
 
     amounts.push(value.amount);
   }
-
-  const animation = getAnimation(data.value);
 
   return {
     type: 'line',
@@ -89,7 +48,9 @@ const getConfig = () => {
       ]
     },
     options: {
-      animation,
+      animation: {
+        duration: 0,
+      },
       responsive: true,
       interaction: {
         intersect: false
@@ -147,7 +108,13 @@ const setConfigAndDraw = () => {
   config.value = getConfig();
 
   if (chart) {
-    chart.destroy();
+    chart.data.labels = config.value.data.labels;
+    chart.data.datasets[0].data = config.value.data.datasets[0].data;
+    chart.data.datasets[1].data = config.value.data.datasets[1].data;
+
+    chart.update('active');
+
+    return;
   }
 
   chart = new Chart(
